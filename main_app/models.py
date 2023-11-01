494c8ab5c85ad9b3242ca7b42d6ebff907969d7a
WalterGuerra <wsteven00@gmail.com>
2023-11-01 16:40:32 -0400

    add finch CRUD and references feeding to ad a feeding to a finch as well as add a toy model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
 # Create your models here.
+
+MEALS = (
+  ('S', 'Seeds'),
+  ('I', 'Insects'),
+  ('F', 'Fruits')
+)
 
 class Finch(models.Model):
   name = models.CharField(max_length=100)
@@ -12,3 +20,33 @@ class Finch(models.Model):
 
   def __str__(self):
     return self.name
+  
+  def get_absolute_url(self):
+      return reverse("finch-detail", kwargs={"finch_id": self.id})
+  
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+  
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]  
+  )
+
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+  
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+      return reverse("toy-detail", kwargs={"pk": self.id})
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
-from .models import Finch 
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch, Toy 
+from .forms import FeedingForm
 
 # Add the following import
 from django.http import HttpResponse
 
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
 
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['habitat', 'food', 'nesting', 'behavior', 'description']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'  
 
 def home(request):
   return render(request, 'home.html')
@@ -18,4 +34,13 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
   finch= Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch})
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('finch-detail', finch_id=finch_id)
